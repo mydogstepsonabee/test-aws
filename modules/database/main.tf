f0bc36af4e5967da09050e916e5364b3c600a2b0
Accurics <admin@accurics.com>
2022-08-08 17:47:41 +0900

    Auto Remediation By Accurics

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -5,16 +5,17 @@ resource "random_password" "password" {
 }
 
 resource "aws_db_instance" "database" {
-  allocated_storage       = 10
-  engine                  = "mysql"
-  engine_version          = "8.0"
-  instance_class          = "db.t2.micro"
-  identifier              = "${var.namespace}-db-instance"
-  name                    = "pets"
-  username                = "admin"
-  password                = random_password.password.result
-  db_subnet_group_name    = var.vpc.database_subnet_group
-  vpc_security_group_ids  = [var.sg.db]
-  skip_final_snapshot     = true
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t2.micro"
+  identifier             = "${var.namespace}-db-instance"
+  name                   = "pets"
+  username               = "admin"
+  password               = random_password.password.result
+  db_subnet_group_name   = var.vpc.database_subnet_group
+  vpc_security_group_ids = [var.sg.db]
+  skip_final_snapshot    = true
   #backup_retention_period = 50
+  backup_retention_period = 30
 }
